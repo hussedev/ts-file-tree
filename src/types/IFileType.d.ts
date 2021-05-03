29ad9d8531f06cea48f8db8f8b72ead1321884aa
huss <hussein.odeh@gmail.com>
2021-05-03 03:41:11 +0200

    Moved and created type interfaces

---
+++ src/types/IFileType.d.ts
@@ -0,0 +1,16 @@
+type IFileType =
+  | 'audio'
+  | 'code'
+  | 'compressed'
+  | 'excel'
+  | 'image'
+  | 'pdf'
+  | 'powerpoint'
+  | 'text'
+  | 'video'
+  | 'word'
+  | 'unknown'
+  | 'folder'
+  | 'openedFolder';
+
+export default IFileType;
