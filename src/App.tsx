61d0caa50c346c4d6a1addaa94f4ff997dededca
huss <hussein.odeh@gmail.com>
2021-05-03 03:43:35 +0200

    feat: added a form to request custom paths, removed context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import ApiClient from './Services/ApiClient';
-import ITree from './ITree';
-import './App.css';
+import React, { useState } from 'react';
+import { getTree } from 'Services/ApiClient';
+import Folder from 'Components/Folder';
+import Form from 'Components/Form';
+import ITree from 'types/ITree';
+import { ReactComponent as BackIcon } from 'assets/back.svg';
 
-export const TreeContext = React.createContext({});
+import 'App.css';
 
 const emptyTree: ITree = {
   path: '',
@@ -14,18 +16,39 @@ const emptyTree: ITree = {
 
 const App: React.FC = () => {
   const [tree, setTree] = useState<ITree>(emptyTree);
+  const [showForm, setShowForm] = useState<boolean>(true);
+  const [formStatus, setFormStatus] = useState<boolean>(true);
 
-  useEffect(() => {
-    ApiClient.getTree().then((fileTree) => {
-      console.log('fileTree', fileTree);
-      setTree(fileTree);
-      console.log('tree', tree);
+  const retrieveTree = (folder: string) => {
+    getTree(folder).then((fileTree) => {
+      if (fileTree) {
+        setFormStatus(true);
+        setTree(fileTree);
+        setShowForm(false);
+      } else {
+        setFormStatus(false);
+      }
     });
-  }, []);
+  };
+
+  const handleBack = () => {
+    setShowForm(true);
+  };
 
   return (
     <div className='App'>
-      <TreeContext.Provider value={{ tree }}>{tree.path}</TreeContext.Provider>
+      <div className='app-container'>
+        <BackIcon
+          display={showForm ? 'none' : 'initial'}
+          className='back-button'
+          onClick={handleBack}
+        />
+        {showForm ? (
+          <Form retrieveTree={retrieveTree} formStatus={formStatus} />
+        ) : (
+          <Folder folder={tree} />
+        )}
+      </div>
     </div>
   );
 };
